cc036f3dd51c3a18e2ccc5d7bfe207f5f8c78af7
TheWorldJar <142929823+TheWorldJar@users.noreply.github.com>
2025-06-30 16:11:51 -0400

    Refactored screen initialization and updated StartEffect to display copyright and navigation options

--- src/main.py
+++ src/main.py
@@ -3,9 +3,11 @@ from asciimatics.scene import Scene
 from asciimatics.exceptions import ResizeScreenError
 from scenes.start_screen import StartEffect
 from scenes.settings import SettingsEffect
+import sys
 
 
-def demo(screen, scene):
+def screen_init(screen, scene):
+    """Initializes th game's screen and its scenes"""
     start_scene = Scene([StartEffect(screen)], -1, name="Start")
     settings_scene = Scene([SettingsEffect(screen)], -1, name="Settings")
     screen.play([start_scene, settings_scene], stop_on_resize=True, start_scene=scene)
@@ -16,7 +18,8 @@ def main():
     last_scene = None
     while True:
         try:
-            Screen.wrapper(demo, arguments=[last_scene])
+            Screen.wrapper(screen_init, arguments=[last_scene])
+            sys.exit(0)
         except ResizeScreenError as e:
             last_scene = e.scene
 
--- src/scenes/start_screen.py
+++ src/scenes/start_screen.py
@@ -1,7 +1,5 @@
-from asciimatics.screen import Screen
-from asciimatics.scene import Scene
 from asciimatics.effects import Print
-from asciimatics.renderers import FigletText
+from asciimatics.renderers import SpeechBubble
 from asciimatics.exceptions import NextScene, StopApplication
 
 
@@ -10,7 +8,9 @@ class StartEffect(Print):
 
     def __init__(self, screen):
         super().__init__(
-            screen=screen, renderer=FigletText("Start Screen", font="big"), y=2
+            screen=screen,
+            renderer=SpeechBubble("Copyright (c) 2025, TheWorldJar"),
+            y=screen.height - 4,
         )
 
     def process_event(self, event):
@@ -20,3 +20,23 @@ class StartEffect(Print):
             elif event.key_code == ord("q"):  # Press 'q' to quit
                 raise StopApplication("User quit")
         return event
+
+    def _update(self, frame_no):
+        # Draw the top bar
+        bar_edge = "=" * (self.screen.width - 2)
+        bar_side = "= + ="
+        bar_content = "Dungeon Depths"
+        self.screen.print_at(bar_edge, 1, 1)
+        self.screen.print_at(bar_side, 1, 2)
+        self.screen.print_at(
+            bar_content, (self._screen.width - len(bar_content)) // 2, 2
+        )
+        self.screen.print_at(bar_side, self._screen.width - 1 - len(bar_side), 2)
+        self.screen.print_at(bar_edge, 1, 3)
+
+        # Draw navigation options
+        nav_options = "[S]ettings [Q]uit"
+        x = (self.screen.width - len(nav_options)) // 2
+        self.screen.print_at(nav_options, x, 5)
+
+        return super()._update(frame_no)
